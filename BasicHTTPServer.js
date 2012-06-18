61d5d4e313577ef7a2fba90983b3714f6c6319ae
IDHeaven <1337kr3wl@gmail.com>
2012-06-18 13:33:27 -0700

    second commit

---
+++ BasicHTTPServer.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello there Basheer I see you finally got it to work\n');
+}).listen(1337);
+
+console.log('Our Glorious server is running at http://127.0.0.1:1337/');
